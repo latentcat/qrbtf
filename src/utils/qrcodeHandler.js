0f66ceea9d5e9dcb600f46361c8eb5a2a76856a8
CPunisher <1343316114@qq.com>
2020-05-01 16:38:42 +0800

    basic QR Code generator

---
+++ src/utils/qrcodeHandler.js
@@ -0,0 +1,29 @@
+import QRCode from "./qrcode";
+
+function extend(target, options) {
+    for (let name in options) {
+        target[name] = options[name]
+    }
+    return target
+}
+
+export function getQrcodeData(options) {
+    if (!options.text || options.text.length <= 0) return null
+
+    options = extend({
+        render		: "canvas",
+        width		: 256,
+        height		: 256,
+        typeNumber	: -1,
+        correctLevel	: 2,
+        background      : "#ffffff",
+        foreground      : "#000000"
+    }, options);
+
+    let qrcode = new QRCode(options.typeNumber, options.correctLevel)
+    qrcode.addData(options.text)
+    qrcode.make()
+
+    return qrcode;
+}
+
