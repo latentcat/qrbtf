3172033a92820dbdd7d9274f172ee93a011580de
ciaochaos <1272777550@qq.com>
2020-05-01 22:06:22 +0800

    0.1.0.2

--- src/components/Qrcode.js
+++ src/components/Qrcode.js
@@ -98,6 +98,19 @@ class Qrcode extends React.Component {
                         </div>
                     </div>
                 </div>
+                <div className="Qr-titled">
+                    <div className="Qr-Centered title-margin">
+                        <div className="Qr-s-title">Downloads</div>
+                        <p className="Qr-s-subtitle">下载二维码</p>
+                    </div>
+                    <div className="Qr-Centered">
+                        <div className="div-btn">
+                            <button className="dl-btn">SVG</button>
+                            <button className="dl-btn">JPG</button>
+                        </div>
+
+                    </div>
+                </div>
             </div>
         );
     }
