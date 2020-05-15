7073895d528dc57f8c3fad26e895c643a3e03478
ciaochaos <1272777550@qq.com>
2020-05-15 17:01:10 +0800

    颜色参数样式调整

--- src/components/param/ParamColor.js
+++ src/components/param/ParamColor.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import reactCSS from 'reactcss';
 import PropTypes from 'prop-types';
-import {TwitterPicker} from 'react-color';
+import { TwitterPicker } from 'react-color';
 import '../Qrcode.css';
 
 const ParamColor = ({ rendererIndex, paramIndex, value, onChange }) => {
@@ -16,8 +16,13 @@ const ParamColor = ({ rendererIndex, paramIndex, value, onChange }) => {
                 display: 'inline-block',
                 cursor: 'pointer',
             },
+            container: {
+                position: 'relative',
+            },
             popover: {
+                marginTop: '10px',
                 position: 'absolute',
+                right: '0',
                 zIndex: '2',
             },
             cover: {
@@ -31,7 +36,7 @@ const ParamColor = ({ rendererIndex, paramIndex, value, onChange }) => {
     });
 
     return (
-        <div>
+        <div style={styles.container}>
             <button className="dl-btn" onClick={ () => setDisplay(!displayColorPicker) }>
                 选择颜色
             </button>
@@ -41,8 +46,9 @@ const ParamColor = ({ rendererIndex, paramIndex, value, onChange }) => {
                         <div style={styles.cover} onClick={() => setDisplay(false)} />
                         <TwitterPicker
                             key={"input_" + rendererIndex + "_" + paramIndex}
-                            triangle="hide"
+                            triangle="none"
                             color={value}
+                            colors={['#FF6900', '#FCB900', '#7BDCB5', '#00D084', '#8ED1FC', '#0693E3', '#ABB8C3', '#EB144C', '#F78DA7', '#9900EF']}
                             onChangeComplete={onChange}
                         />
                     </div>
