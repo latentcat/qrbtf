9220aafacfdbe57cc490e97b63586a1332ee48aa
CPunisher <1343316114@qq.com>
2020-07-01 23:48:43 +0800

    GA for zoom

--- src/containers/app/ImageZoom.js
+++ src/containers/app/ImageZoom.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import {handleZoom} from "../../utils/gaHelper";
 
-const ImageZoom = (props) => {
-    const zoomRef = React.useRef(props.zoom.clone(props));
+const ImageZoom = ({ zoom, ...other }) => {
+    const zoomRef = React.useRef(zoom.clone(other));
+    zoomRef.current.on('open', event => {handleZoom(event.target.src)})
+
 
     function attachZoom(image) {
         zoomRef.current.attach(image);
     }
 
-    return <img ref={attachZoom} {...props} />
+    return <img ref={attachZoom} {...other} />
 };
 
 export default ImageZoom;
