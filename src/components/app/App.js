e5aae8511c08e66a1eb481dc9eaee5454158d130
ciaochaos <1272777550@qq.com>
2020-07-02 13:40:33 +0800

    GA 更新

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -13,7 +13,21 @@ import {loadDownloadData} from "../../actions";
 import ReactGA from 'react-ga';
 import {setScrollbarWidthProp} from "../../utils/util"
 
-ReactGA.initialize('UA-165845289-1');
+ReactGA.initialize(
+    [
+        {
+            trackingId: 'UA-165845289-1',
+            gaOptions: {
+                name: 'trackerUA',
+            }
+        },
+        {
+            trackingId: 'G-3NKS6ZG27V',
+            gaOptions: { name: 'trackerG' }
+        }
+    ],
+    { debug: true, alwaysSendToDefaultTracker: false }
+);
 function App({ dispatch }) {
     const updateDownloadData = useCallback((downloadData) => dispatch(loadDownloadData(downloadData)), []);
     setScrollbarWidthProp()
