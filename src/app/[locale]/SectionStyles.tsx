8814c4d95c258499fd23480b4be25372ed5afba9
ciaochaos <1272777550@qq.com>
2024-03-12 13:27:54 +0800

    Update SectionStyles.tsx

--- src/app/[locale]/SectionStyles.tsx
+++ src/app/[locale]/SectionStyles.tsx
@@ -7,21 +7,29 @@ const styleList = [0, 1, 2, 3, 4, 5, 6]
 export function SectionStyles() {
   return (
     <div className="mt-9">
-      <div className="overflow-x-auto no-scrollbar">
+      <div className="overflow-x-auto no-scrollbar snap-x sm:snap-none snap-mandatory">
         <div className="flex flex-col">
-          <Container>
-            <div className="flex gap-3">
-              {styleList.map((item, index) => (
-                <div key={index}>
-                  <div className="w-[calc((100vw-(12px)*5)/2)] sm:w-48 border rounded-xl bg-accent/30">
-                    <AspectRatio ratio={1} />
-                  </div>
-                </div>
-              ))}
 
-              <div className="w-3 shrink-0" />
+          <div className="w-full flex flex-col items-center sm:px-6 lg:px-12">
+            <div className="w-full max-w-5xl">
+              <div className="flex sm:gap-3">
+
+                <div className="w-3 shrink-0 sm:hidden"/>
+
+                {styleList.map((item, index) => (
+                  <div key={index} className="snap-start pl-6 -ml-3 sm:pl-0 sm:ml-0">
+                    <div className="w-[calc((100vw-(12px)*5)/2)] sm:w-48 border rounded-xl bg-accent/30">
+                      <AspectRatio ratio={1}/>
+                    </div>
+                  </div>
+                ))}
+
+                <div className="w-3 shrink-0"/>
+
+              </div>
             </div>
-          </Container>
+          </div>
+
         </div>
       </div>
     </div>
