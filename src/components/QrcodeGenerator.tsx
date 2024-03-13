84401edb218a14316020049d39545194b9b25ce0
ciaochaos <1272777550@qq.com>
2024-03-14 06:06:23 +0800

    update

--- src/components/QrcodeGenerator.tsx
+++ src/components/QrcodeGenerator.tsx
@@ -71,55 +71,59 @@ export function QrcodeGenerator<P extends {}>(props: QrcodeGeneratorProps<P>) {
 
 
           <div className="grow">
-            <SectionTitle title={props.title} subtitle={props.subtitle} />
+            <div className="sticky top-9">
+              <SectionTitle title={props.title} subtitle={props.subtitle} />
 
-            <div>
+              <div>
 
-              <Form {...form}>
-                <form className="not-prose divide-y">
-                  {params.map((param, index) => (
-                    <div
-                      key={param.name}
-                      className="py-1.5 flex flex-col items-stretch justify-center min-h-[60px]"
-                    >
-                      {renderControls(param)}
-                    </div>
-                  ))}
-                </form>
-              </Form>
+                <Form {...form}>
+                  <form className="not-prose divide-y">
+                    {params.map((param, index) => (
+                      <div
+                        key={param.name}
+                        className="py-1.5 flex flex-col items-stretch justify-center min-h-[60px]"
+                      >
+                        {renderControls(param)}
+                      </div>
+                    ))}
+                  </form>
+                </Form>
+              </div>
             </div>
           </div>
 
           <div className="shrink-0 w-full sm:w-[396px] md:w-72 lg:w-[396px]">
-            <Button className="w-full mb-6 hidden">
-              {t('generate')}
-            </Button>
-            <div className="">
+            <div className="sticky top-9">
+              <Button className="w-full mb-6 hidden">
+                {t('generate')}
+              </Button>
+              <div className="">
 
-              <Label
-                className="flex items-center justify-between mb-1.5"
-                htmlFor="output_image"
-              >
-                {t('qrcode_output')}
-                <Badge
-                  // onClick={() => downloadImage()}
-                  className={cn(
-                    "rounded-md hover:bg-accent cursor-pointer",
-                    false ? "" : "opacity-50 pointer-events-none"
-                  )}
-                  variant="outline"
+                <Label
+                  className="flex items-center justify-between mb-1.5"
+                  htmlFor="output_image"
                 >
-                  <LucideDownload className="w-4 h-4 mr-1"/>{t('download')}
-                </Badge>
-              </Label>
-              <div className="relative border rounded-xl bg-accent/30 w-full overflow-hidden">
-                <AspectRatio ratio={1}/>
-                <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center">
-                  <QrCodeIcon className="w-12 h-12 opacity-20"/>
-                </div>
-                <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center">
-                  <div>
-                    {props.qrcodeModule.renderer(componentProps)}
+                  {t('qrcode_output')}
+                  <Badge
+                    // onClick={() => downloadImage()}
+                    className={cn(
+                      "rounded-md hover:bg-accent cursor-pointer",
+                      false ? "" : "opacity-50 pointer-events-none"
+                    )}
+                    variant="outline"
+                  >
+                    <LucideDownload className="w-4 h-4 mr-1"/>{t('download')}
+                  </Badge>
+                </Label>
+                <div className="relative border rounded-xl bg-accent/30 w-full overflow-hidden">
+                  <AspectRatio ratio={1}/>
+                  <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center">
+                    <QrCodeIcon className="w-12 h-12 opacity-20"/>
+                  </div>
+                  <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center">
+                    <div>
+                      {props.qrcodeModule.renderer(componentProps)}
+                    </div>
                   </div>
                 </div>
               </div>
