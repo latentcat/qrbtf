36337a1fbe90da915e83cb113765bc2a41348125
CPunisher <1343316114@qq.com>
2024-03-14 13:39:12 +0800

    Fix error on LocaleSwitcher

--- src/components/LocaleSwitcherSelect.tsx
+++ src/components/LocaleSwitcherSelect.tsx
@@ -1,10 +1,10 @@
-'use client';
+"use client";
 
-import clsx from 'clsx';
-import {useParams} from 'next/navigation';
-import {ChangeEvent, ReactNode, useTransition} from 'react';
-import {useRouter, usePathname} from '@/navigation';
-import {ChevronUpDownIcon} from "@heroicons/react/20/solid";
+import clsx from "clsx";
+import { useParams } from "next/navigation";
+import { ChangeEvent, ReactNode, useTransition } from "react";
+import { useRouter, usePathname } from "@/navigation";
+import { ChevronUpDownIcon } from "@heroicons/react/20/solid";
 
 type Props = {
   children: ReactNode;
@@ -13,34 +13,27 @@ type Props = {
 };
 
 export default function LocaleSwitcherSelect({
-                                               children,
-                                               defaultValue,
-                                               label
-                                             }: Props) {
+  children,
+  defaultValue,
+  label,
+}: Props) {
   const router = useRouter();
   const [isPending, startTransition] = useTransition();
   const pathname = usePathname();
-  const params = useParams();
 
   function onSelectChange(event: ChangeEvent<HTMLSelectElement>) {
     const nextLocale = event.target.value;
-    console.log(nextLocale)
+    console.log(nextLocale);
     startTransition(() => {
-      router.replace(
-        // @ts-expect-error -- TypeScript will validate that only known `params`
-        // are used in combination with a given `pathname`. Since the two will
-        // always match for the current route, we can skip runtime checks.
-        {pathname, params},
-        {locale: nextLocale}
-      );
+      router.replace(pathname, { locale: nextLocale });
     });
   }
 
   return (
     <label
       className={clsx(
-        'relative flex items-center',
-        isPending && 'transition-opacity [&:disabled]:opacity-30'
+        "relative flex items-center",
+        isPending && "transition-opacity [&:disabled]:opacity-30"
       )}
     >
       <p className="sr-only">{label}</p>
