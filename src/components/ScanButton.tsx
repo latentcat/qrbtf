9308a8a4117bc248ebe95be86ef31e0055efcfc3
CPunisher <1343316114@qq.com>
2024-03-14 13:05:45 +0800

    Fix double-trigger issue on scan button

--- src/components/ScanButton.tsx
+++ src/components/ScanButton.tsx
@@ -1,24 +1,32 @@
-"use client"
+"use client";
 
-import {Badge} from "@/components/ui/badge";
-import {LucideScan} from "lucide-react";
-import {useRef} from "react";
-
+import { Badge } from "@/components/ui/badge";
+import { LucideScan } from "lucide-react";
+import { useRef } from "react";
 
 export function ScanButton(props: { name: string }) {
-
   const scanRef = useRef<HTMLInputElement>(null);
 
   return (
     <>
-      <input ref={scanRef} id="qr-input-file" type="file" accept="image/*" className="hidden"/>
+      <input
+        ref={scanRef}
+        id="qr-input-file"
+        type="file"
+        accept="image/*"
+        className="hidden"
+      />
       <Badge
-        onClick={() => scanRef.current?.click()}
+        onClick={(evt) => {
+          evt.preventDefault();
+          scanRef.current?.click();
+        }}
         className="rounded-md hover:bg-accent cursor-pointer"
         variant="outline"
       >
-        <LucideScan className="w-4 h-4 mr-1"/>{props.name}
+        <LucideScan className="w-4 h-4 mr-1" />
+        {props.name}
       </Badge>
     </>
-  )
+  );
 }
