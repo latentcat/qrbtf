d5f2d647a8a443e2630b0770b1bf69084f5f4ce0
CPunisher <1343316114@qq.com>
2024-03-15 21:01:11 +0800

    Finish c2

--- src/lib/qrbtf_lib/qrcodes/c2.tsx
+++ src/lib/qrbtf_lib/qrcodes/c2.tsx
@@ -4,90 +4,223 @@ import {
   QrbtfRendererCommonProps,
   QrbtfModule,
 } from "@/lib/qrbtf_lib/qrcodes/common";
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { QRPointType, encode } from "../encoder";
+import { gamma } from "@/lib/utils";
 
 interface RenderC2OwnProps {
   contrast: number;
   brightness: number;
   background: string;
+  align_type: "none" | "black-white";
+  timing_type: "none" | "black-white";
 }
 
 export type QrbtfRendererC2Props = RenderC2OwnProps & QrbtfRendererCommonProps;
 
 function QrbtfRendererC2(props: QrbtfRendererC2Props) {
+  const { align_type, timing_type, contrast, brightness, background } = props;
   const [table, typeTable] = useMemo(() => encode("qrbtf"), []);
+
   const points = useMemo(() => {
     const points: React.ReactNode[] = [];
     for (let x = 0; x < table.length; x++) {
       for (let y = 0; y < table.length; y++) {
+        const posX = 3 * x;
+        const posY = 3 * y;
         switch (typeTable[x][y]) {
           case QRPointType.EMPTY:
             continue;
           case QRPointType.ALIGN_CENTER:
           case QRPointType.ALIGN_OTHER:
-          case QRPointType.TIMING:
-            points.push(
-              <rect
-                opacity={1}
-                width={1}
-                height={1}
-                key={points.length}
-                fill="#0000FF"
-                x={x}
-                y={y}
-              />,
-            );
+            if (table[x][y]) {
+              if (align_type === "black-white") {
+                points.push(
+                  <use
+                    key={points.length}
+                    xlinkHref="#B-black"
+                    x={posX - 0.03}
+                    y={posY - 0.03}
+                  />,
+                );
+              } else {
+                points.push(
+                  <use
+                    key={points.length}
+                    xlinkHref="#S-black"
+                    x={posX + 1 - 0.01}
+                    y={posY + 1 - 0.01}
+                  />,
+                );
+              }
+            } else {
+              if (align_type === "none") {
+                points.push(
+                  <use
+                    key={points.length}
+                    xlinkHref="#S-white"
+                    x={posX + 1}
+                    y={posY + 1}
+                  />,
+                );
+              } else {
+                points.push(
+                  <use
+                    key={points.length}
+                    xlinkHref="#B-white"
+                    x={posX - 0.03}
+                    y={posY - 0.03}
+                  />,
+                );
+              }
+            }
             break;
+          case QRPointType.TIMING:
+            if (table[x][y]) {
+              if (timing_type === "black-white") {
+                points.push(
+                  <use
+                    key={points.length}
+                    xlinkHref="#B-black"
+                    x={posX - 0.03}
+                    y={posY - 0.03}
+                  />,
+                );
+              } else {
+                points.push(
+                  <use
+                    key={points.length}
+                    xlinkHref="#S-black"
+                    x={posX + 1}
+                    y={posY + 1}
+                  />,
+                );
+              }
+            } else {
+              if (timing_type === "none") {
+                points.push(
+                  <use
+                    key={points.length}
+                    xlinkHref="#S-white"
+                    x={posX + 1}
+                    y={posY + 1}
+                  />,
+                );
+              } else {
+                points.push(
+                  <use
+                    key={points.length}
+                    xlinkHref="#B-white"
+                    x={posX - 0.03}
+                    y={posY - 0.03}
+                  />,
+                );
+              }
+            }
           case QRPointType.POS_CENTER:
             points.push(
-              <rect
-                width={1}
-                height={1}
+              <use
                 key={points.length}
-                fill="#00FF00"
-                x={x}
-                y={y}
+                fill="black"
+                xlinkHref="#B"
+                x={posX - 0.03}
+                y={posY - 0.03}
               />,
             );
             break;
           case QRPointType.POS_OTHER:
-            points.push(
-              <rect
-                width={1}
-                height={1}
-                key={points.length}
-                fill="#FF0000"
-                x={x}
-                y={y}
-              />,
-            );
+            if (table[x][y]) {
+              points.push(
+                <use
+                  key={points.length}
+                  fill="black"
+                  xlinkHref="#B"
+                  x={posX - 0.03}
+                  y={posY - 0.03}
+                />,
+              );
+            } else {
+              points.push(
+                <use
+                  key={points.length}
+                  xlinkHref="#B-white"
+                  x={posX - 0.03}
+                  y={posY - 0.03}
+                />,
+              );
+            }
             break;
           default:
             points.push(
-              <rect
-                opacity={1}
-                width={1}
-                height={1}
+              <use
                 key={points.length}
-                fill="#00FFFF"
-                x={x}
-                y={y}
+                xlinkHref="#S-black"
+                x={posX + 1}
+                y={posY + 1}
               />,
             );
         }
       }
     }
     return points;
-  }, [table.length, typeTable]);
+  }, [align_type, table, timing_type, typeTable]);
 
-  const viewBox = `${-table.length / 5} ${-table.length / 5} ${
-    (7 * table.length) / 5
-  } ${(7 * table.length) / 5}`;
+  const [grayPoints, setGrayPoints] = useState<React.ReactNode[]>([]);
+  useEffect(() => {
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    const img = document.createElement("img");
+    const gpl: React.ReactNode[] = [];
+
+    canvas.style.imageRendering = "pixelated";
+    const size = table.length * 3;
+
+    img.src = background;
+    img.onload = () => {
+      canvas.width = size;
+      canvas.height = size;
+
+      if (!ctx) {
+        return;
+      }
+
+      ctx.imageSmoothingEnabled = false;
+      ctx.drawImage(img, 0, 0, size, size);
+
+      for (let x = 0; x < canvas.width; x++) {
+        for (let y = 0; y < canvas.height; y++) {
+          const imageData = ctx.getImageData(x, y, 1, 1);
+          const data = imageData.data;
+          const gray = gamma(data[0], data[1], data[2]);
+          if (
+            Math.random() >
+              (gray / 255 + brightness - 0.5) * (contrast + 1) + 0.5 &&
+            (x % 3 !== 1 || y % 3 !== 1)
+          )
+            gpl.push(
+              <use key={"g_" + x + "_" + y} x={x} y={y} xlinkHref="#S-black" />,
+            );
+        }
+      }
+      setGrayPoints(gpl);
+    };
+  }, [background, brightness, contrast, table.length]);
+
+  const viewBox = `${(-table.length * 3) / 5} ${(-table.length * 3) / 5} ${
+    (21 * table.length) / 5
+  } ${(21 * table.length) / 5}`;
 
   return (
     <svg viewBox={viewBox} {...props}>
-      {points}
+      <defs>
+        <rect id="B-black" fill="black" width={3.08} height={3.08} />
+        <rect id="B-white" fill="white" width={3.08} height={3.08} />
+        <rect id="S-black" fill="black" width={1.02} height={1.02} />
+        <rect id="S-white" fill="white" width={1.02} height={1.02} />
+        <rect id="B" width={3.08} height={3.08} />
+        <rect id="S" width={1.02} height={1.02} />
+      </defs>
+      {[...grayPoints, points]}
     </svg>
   );
 }
