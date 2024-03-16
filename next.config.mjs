ab56a1f6db295e2adb11aaebf4efd294487e919a
ciaochaos <1272777550@qq.com>
2024-03-16 21:49:02 +0800

    update

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
   async rewrites() {
     return [
       {
--- src/middleware.ts
+++ src/middleware.ts
@@ -18,6 +18,6 @@ export const config = {
 
     // Enable redirects that add missing locales
     // (e.g. `/pathnames` -> `/en/pathnames`)
-    "/((?!_next|_vercel|api|.*\\..*).*)",
+    "/((?!_next|_vercel|api|mp|.*\\..*).*)",
   ],
 };
