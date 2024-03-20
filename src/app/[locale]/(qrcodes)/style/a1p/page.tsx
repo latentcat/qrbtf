88b31a3f37279742c4758f94dad03e24173013f6
CPunisher <1343316114@qq.com>
2024-03-20 16:51:36 +0800

    Apply a1 preset to finish a1c and a1p

--- src/app/[locale]/(qrcodes)/style/a1p/page.tsx
+++ src/app/[locale]/(qrcodes)/style/a1p/page.tsx
@@ -1,3 +1,22 @@
+import QrcodeGeneratorWithProvider from "@/components/QrcodeGeneratorWithProvider";
+import {
+  QrbtfRendererA1Props,
+  qrbtfModuleA1,
+} from "@/lib/qrbtf_lib/qrcodes/a1";
+import { useA1Params } from "@/lib/qrbtf_lib/qrcodes/a1_config";
+import { useTranslations } from "next-intl";
+
 export default function Page() {
-  return <div></div>;
+  const t = useTranslations("qrcodes.a1");
+  const { params, defaultValues } = useA1Params("a1p");
+
+  return (
+    <QrcodeGeneratorWithProvider<QrbtfRendererA1Props>
+      title={t("title")}
+      subtitle={t("subtitle")}
+      qrcodeModule={qrbtfModuleA1}
+      params={params}
+      defaultValues={defaultValues}
+    />
+  );
 }
