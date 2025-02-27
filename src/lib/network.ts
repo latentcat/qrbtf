36f750f0329749b84a1581e907f6e3a82ef395ad
CPunisher <1343316114@qq.com>
2025-02-27 14:19:54 +0800

    Add application/json for http

--- src/lib/network.ts
+++ src/lib/network.ts
@@ -1,7 +1,13 @@
 import { toast } from "sonner";
 
 export const http: typeof fetch = async (input, init) => {
-  const res = await fetch(input, init);
+  const res = await fetch(input, {
+    headers: {
+      "Content-Type": "application/json",
+      ...init?.headers,
+    },
+    ...init,
+  });
   if (!res.ok) {
     const body = await res.json();
     const msg = body["error"] || "Network Error";
